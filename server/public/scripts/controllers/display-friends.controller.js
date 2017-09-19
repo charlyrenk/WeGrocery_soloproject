341fbe0fe7919c3b10451c9980a551e773dff2f7
Charles Renk <renkx008@umn.edu>
2017-09-19 16:26:55 -0500

    accepting a friend request now updates both user friend lists

--- server/public/scripts/controllers/display-friends.controller.js
+++ server/public/scripts/controllers/display-friends.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('DisplayFriendsController', ['UserService', 'DisplayFriendsService', function(UserService, DisplayFriendsService) {
+myApp.controller('DisplayFriendsController', ['UserService', 'DisplayFriendsService', 'ListService', function(UserService, DisplayFriendsService, ListService) {
     console.log('DisplayFriendsController loaded');
     var vm = this;
 
@@ -6,6 +6,7 @@ myApp.controller('DisplayFriendsController', ['UserService', 'DisplayFriendsServ
 
     vm.userObject = UserService.userObject;
     vm.getThisFriendsLists = DisplayFriendsService.getThisFriendsLists;
+    vm.changeItemStatus = ListService.changeItemStatus;
 
    
 }]);
--- server/routes/friends.router.js
+++ server/routes/friends.router.js
@@ -53,31 +53,51 @@ router.post('/acceptRequest', function (req, res) {
     var currentUser = req.body.currentUser
     var currentUserId = req.body.currentUser.id
     var allRequestsObject = req.body.allRequestsObject
-    
+
     console.log('Friend post initiated:', userToAdd, currentUserId)
 
     // User.findByIdAndUpdate(currentUserId, {
     //     $set: userToAdd
-        
+
     // })
     User.findById(currentUserId, function (err, user) {
         if (err) return handleError(err);
-        
+
         //console.log('Basket Index server: ', user.basket[basketIndex].quantity)
         user.pendingFriendRequests = allRequestsObject //$inc
         user.save(function (err) {
-          if (err) { 
-            return handleError(err) 
-          };
+            if (err) {
+                return handleError(err)
+            };
         });
-      });
+    });
 
-  
+
 
     User.findByIdAndUpdate(currentUserId, {
             $push: {
-                friendsList: userToAdd
+                friendsList: {
+                    id: otherUserId,
+                    userName: userToAdd.userName
+                }
             }
+        },
+        function (err, data) {
+            if (err) {
+                console.log('update error: ', err);
+
+                res.sendStatus(500);
+            }
+        }
+    )
+
+    User.findByIdAndUpdate(otherUserId, {
+            $push: {
+                    friendsList: {
+                        id: currentUserId,
+                        userName: currentUser.userName
+                    }
+                }
         },
         function (err, data) {
             if (err) {
